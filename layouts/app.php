c6cc9d0c09a3f7e1b4f15730c8e3e668783482e6
Arlan Tri Handika <handikaarlan@gmail.com>
2024-06-19 14:26:17 +0700

    add favicon

--- layouts/app.php
+++ layouts/app.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo isset($title) ? $title : 'Default Title'; ?></title>
     <link rel="stylesheet" href="assets/styles/style.css">
+    <link rel="shortcut icon" href="assets/images/favicon.png" type="image/x-icon">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Kanit&display=swap" rel="stylesheet">
