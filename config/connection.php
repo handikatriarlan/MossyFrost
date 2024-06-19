4067a6463ee7c683fc733f50c79246375237beb8
Arlan Tri Handika <handikaarlan@gmail.com>
2024-06-19 12:17:47 +0700

    fix connection

--- config/connection.php
+++ config/connection.php
@@ -4,4 +4,10 @@ $username = "root";
 $password = "";
 $db = "db_mossyfrost";
 
-$conn = new PDO("mysql:host=$server;dbname=$db", $username, $password);
+try {
+    $conn = new PDO("mysql:host=$server;dbname=$db", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected!\n";
+} catch (PDOException $e) {
+    echo "Koneksi gagal: " . $e->getMessage();
+}
