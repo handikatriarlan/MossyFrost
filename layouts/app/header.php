b04cc032c608b2cab80728500070f01a5fe9bc34
Arlan Tri Handika <handikaarlan@gmail.com>
2024-06-19 14:07:18 +0700

    fix navbar

--- layouts/app/header.php
+++ layouts/app/header.php
@@ -5,17 +5,16 @@
             <li class="brand"><a href="index.php">Mossy Frost</a></li>
             <li class="align-right"><a href="index.php">Beranda</a></li>
             <li class="align-right"><a href="menu.php">Menu</a></li>
-            <li class="align-right"><a href="order.php">Pesan</a></li>
-            <li class="align-right"><a href="history.php">Riwayat</a></li>
-            <li class="align-right"><a href="contact.php">Kontak</a></li>
-
             <?php if (isset($_SESSION['user_id'])) { ?>
+                <li class="align-right"><a href="order.php">Pesan</a></li>
+                <li class="align-right"><a href="history.php">Riwayat</a></li>
+                <li class="align-right"><a href="contact.php">Kontak</a></li>
                 <li class="align-right"><a href="logout.php">Keluar</a></li>
             <?php } else { ?>
+                <li class="align-right"><a href="contact.php">Kontak</a></li>
                 <li class="align-right"><a href="login.php">Masuk</a></li>
                 <li class="align-right"><a href="register.php">Daftar</a></li>
             <?php } ?>
-
         </ul>
     </nav>
 </header>
