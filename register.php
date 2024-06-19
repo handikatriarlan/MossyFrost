216d16d4361b58059ab6b6a78ac6448f38587447
Arlan Tri Handika <handikaarlan@gmail.com>
2024-06-19 12:04:08 +0700

    add style

--- register.php
+++ register.php
@@ -4,29 +4,29 @@ ob_start();
 ?>
 
 <main class="login-page">
-    <form class="login-form" action="#">
+    <form class="login-form" action="#" method="post">
         <h2>Daftar Akun</h2>
         <label for="name">Nama:</label>
-        <input type="text" id="name" name="name">
+        <input type="text" id="name" name="name" required>
 
         <label for="email">E-mail:</label>
-        <input type="email" id="email" name="email">
+        <input type="email" id="email" name="email" required>
 
         <label for="phone">Nomor HP:</label>
-        <input type="number" id="phone" name="phone">
+        <input type="number" id="phone" name="phone" required>
 
         <label for="address">Alamat:</label>
-        <textarea name="address" id="address">Alamat:</textarea>
+        <textarea name="address" id="address" required></textarea>
 
         <label for="password">Password:</label>
-        <input type="password" id="password" name="password">
+        <input type="password" id="password" name="password" required>
 
-        <label for="password">Ulangi Password:</label>
-        <input type="password" id="password" name="password">
+        <label for="confirm_password">Ulangi Password:</label>
+        <input type="password" id="confirm_password" name="confirm_password" required>
 
         <input type="submit" value="Daftar">
 
-        <p>Sudah memiliki akun?<a href="login.php"> Masuk</a></p>
+        <p>Sudah memiliki akun? <a href="login.php">Masuk</a></p>
     </form>
 </main>
 
