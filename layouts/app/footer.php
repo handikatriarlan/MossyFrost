d005ae812ef27f2f9f9f7c0936112d98387553c4
Arlan Tri Handika <handikaarlan@gmail.com>
2024-06-21 21:21:14 +0700

    update footer

--- layouts/app/footer.php
+++ layouts/app/footer.php
@@ -1,3 +1,4 @@
 <footer>
     <p>&copy;2024 Mossy Frost. Semua hak cipta dilindungi undang-undang.</p>
+    <p class="creator">Dibuat dengan ♡ oleh: <a href="https://handikatriarlan.w3spaces.com/" target="_blank">@handikatriarlan</a></p>
 </footer>
