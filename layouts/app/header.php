cde0ef09b60e639572d4df3e079d9783dce13462
Arlan Tri Handika <handikaarlan@gmail.com>
2024-06-21 22:27:11 +0700

    fix navbar

--- layouts/app/header.php
+++ layouts/app/header.php
@@ -4,13 +4,21 @@
         <ul>
             <li class="brand"><a href="index.php">Mossy Frost</a></li>
             <li class="align-right"><a href="index.php">Beranda</a></li>
-            <li class="align-right"><a href="menu.php">Menu</a></li>
-            <li class="align-right"><a href="order.php">Pesan</a></li>
-            <?php if (isset($_SESSION['user_id'])) { ?>
+            <?php if (isset($_SESSION['user_id']) && $_SESSION['role'] == 'user') { ?>
+                <li class="align-right"><a href="menu.php">Menu</a></li>
+                <li class="align-right"><a href="order.php">Pesan</a></li>
                 <li class="align-right"><a href="history.php">Riwayat</a></li>
                 <li class="align-right"><a href="contact.php">Kontak</a></li>
                 <li class="align-right"><a href="logout.php">Keluar</a></li>
+            <?php } else if (isset($_SESSION['user_id']) && $_SESSION['role'] == 'admin') { ?>
+                <li class="align-right"><a href="admin/data-user.php">Data Pengguna</a></li>
+                <li class="align-right"><a href="admin/data-menu.php">Data Menu</a></li>
+                <li class="align-right"><a href="admin/data-order.php">Data Pesan</a></li>
+                <li class="align-right"><a href="admin/data-contact.php">Data Kontak</a></li>
+                <li class="align-right"><a href="logout.php">Keluar</a></li>
             <?php } else { ?>
+                <li class="align-right"><a href="menu.php">Menu</a></li>
+                <li class="align-right"><a href="order.php">Pesan</a></li>
                 <li class="align-right"><a href="contact.php">Kontak</a></li>
                 <li class="align-right"><a href="login.php">Masuk</a></li>
                 <li class="align-right"><a href="register.php">Daftar</a></li>
