6e5bc5950e4dc0b5ac5eb8a14519fb637073a0e2
Arlan Tri Handika <handikaarlan@gmail.com>
2024-06-25 22:55:15 +0700

    add data user

--- data-user.php
+++ data-user.php
@@ -10,10 +10,44 @@ if (!isset($_SESSION['user_id'])) {
 
 $title = "Mossy Frost - Data Pengguna";
 ob_start();
+
+$sql = "SELECT id, email, name, phone, address, role FROM users WHERE role = 'user'";
+$stmt = $conn->prepare($sql);
+$stmt->execute();
+$users = $stmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
 
 <main>
     <h1 class="title">Data Pengguna</h1>
+    <a href="data-user-add.php" class="btn-add">Tambah Pengguna</a>
+    <table class="data-table">
+        <thead>
+            <tr>
+                <th>#</th>
+                <th>Email</th>
+                <th>Nama</th>
+                <th>Telepon</th>
+                <th>Alamat</th>
+                <th>Aksi</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php $no = 1; ?>
+            <?php foreach ($users as $user) : ?>
+                <tr>
+                    <td><?php echo $no++; ?></td>
+                    <td><?php echo htmlspecialchars($user['email']); ?></td>
+                    <td><?php echo htmlspecialchars($user['name']); ?></td>
+                    <td><?php echo htmlspecialchars($user['phone']); ?></td>
+                    <td><?php echo htmlspecialchars($user['address']); ?></td>
+                    <td>
+                        <a href="data_user_edit.php?id=<?php echo $user['id']; ?>" class="btn-edit">Edit</a>
+                        <a href="delete_user.php?id=<?php echo $user['id']; ?>" class="btn-delete" onclick="return confirm('Apakah Anda yakin ingin menghapus pengguna ini?');">Hapus</a>
+                    </td>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
 </main>
 
 <?php
